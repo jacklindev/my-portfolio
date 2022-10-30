f78d99b12281004caf502c461f12ba827f1c135f
jackliudev <jackliuprodev@gmail.com>
2022-10-30 06:28:20 -0400

    add experience data

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,42 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Teton Private Limited',
-        jobtitle: 'Software Engineer I',
-        startYear: 'Jan 2022',
-        endYear: 'Present'
+        company: 'Mighty Networks',
+        jobtitle: 'Front End Developer',
+        startYear: '2020',
+        endYear: '2022',
+        skills: 'React, React Native, TypeScript, TailwindCSS, Jest, AWS'
     },
     {
         id: 2,
-        company: 'Fiverr(freelance)',
+        company: 'Lift & Co. Expo',
         jobtitle: 'Full-Stack Developer',
-        startYear: 'Jun 2021',
-        endYear: 'Jan 2022'
+        startYear: '2018',
+        endYear: '2020',
+        skills: 'React, Redux, Jest, Contentful, TypeScript, Mailchimp'
     },
     {
         id: 3,
-        company: 'Learning New Technology For Everyday',
-        jobtitle: 'Self Learning',
-        startYear: '2018',
-        endYear: 'Present'
+        company: 'Collins Harper',
+        jobtitle: 'Full Stack Developer',
+        startYear: '2015',
+        endYear: '2017',
+        skills: 'React, React Native, Redux, Node.js, Express.js, AWS'
+    },
+    {
+        id: 4,
+        company: 'Concept Coders',
+        jobtitle: 'Software Engineer',
+        startYear: '2014',
+        endYear: '2015',
+        skills: 'HTML, CSS, jQuery, JavaScript, AngularJS, Wordpress, PHP, Laravel'
+    },
+    {
+        id: 5,
+        company: 'Bronron Apps',
+        jobtitle: 'Software Developer',
+        startYear: '2012',
+        endYear: '2013',
+        skills: 'HTML, CSS, jQuery, JavaScript, Wordpress, PHP, Laravel'
     },
 ]
