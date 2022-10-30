a56c32599562e8076519e7414f605b64e6fc83f3
jackliudev <jackliuprodev@gmail.com>
2022-10-30 06:16:09 -0400

    download CV

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am Jack Lin, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: '/me.JPG',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1-iKBzgch6TWXEppY7-wmyMHVWE2IGG_s/view?usp=sharing'
 }
