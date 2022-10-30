5cdb42887fe640237c5ed20aae636ab75a1192cc
jackliudev <jackliuprodev@gmail.com>
2022-10-30 09:31:14 -0400

    add projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,88 +1,89 @@
-import eight from '../assets/svg/projects/eight.svg'
-import five from '../assets/svg/projects/five.svg'
-import four from '../assets/svg/projects/four.svg'
-import one from '../assets/svg/projects/one.svg'
-import seven from '../assets/svg/projects/seven.svg'
-import six from '../assets/svg/projects/six.svg'
-import three from '../assets/svg/projects/three.svg'
-import two from '../assets/svg/projects/two.svg'
-
+import mighty from "../assets/png/projects/mighty.png";
+import ernjarvis from "../assets/png/projects/ernjarvis.svg";
+import sports from "../assets/png/projects/sports.gif";
+import aerotime from "../assets/png/projects/aerotime.jpg";
+import pully from "../assets/png/projects/pully.svg";
+import varagon from "../assets/png/projects/varagon.jpg"
+import industrie from "../assets/png/projects/industrie.svg";
+import liftco from "../assets/png/projects/liftco.png";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
-        code: '',
-        demo: 'https://www.aylanetworks.com/',
-        image: six
-    },
-    {
-        id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
-        code: 'https://github.com/jackliudev/jewelry-niche',
-        demo: 'https://pandora-web.netlify.app/',
-        image: seven
-    },
-    {
-        id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/jackliudev/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
-        image: eight
-    },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: two
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
-    },
-]
+  {
+    id: 1,
+    projectName: "Mighty networks",
+    projectDesc: " Built Communities, Courses & Memberships.",
+    tags: ["React", "React Native", "TypeScript", "Redux"],
+    code: "",
+    demo: "https://www.mightynetworks.com/",
+    image: mighty,
+  },
 
+  {
+    id: 3,
+    projectName: "Sports betters",
+    projectDesc: "Automated Bet Management",
+    tags: ["Vue", "Vuex", "TypeScript"],
+    code: "",
+    demo: "https://www.torneo.ca/",
+    image: sports,
+  },
+  {
+    id: 4,
+    projectName: "ernJARVIS",
+    projectDesc: "Bitcoin & Crypto TAMP and SMA network",
+    tags: ["React", "Charts.js", "Web3.js"],
+    code: "",
+    demo: "https://earnjarvis.com/",
+    image: ernjarvis,
+  },
+  {
+    id: 5,
+    projectName: "AEROTIME",
+    projectDesc:
+      "A gorgeous calendar that continuously optimizes for minimizing context switches and better prioritization",
+    tags: ["React", "Redux", "D3.js", "TypeScript"],
+    code: "",
+    demo: "https://www.aerotime.com/",
+    image: aerotime,
+  },
+  {
+    id: 6,
+    projectName: "Pulley",
+    projectDesc: "The top-rated cap table solutions",
+    tags: ["React", "Redux", "React-query", "Ant Design"],
+    code: "",
+    demo: "https://pulley.com/",
+    image: pully,
+  },
+  {
+    id: 7,
+    projectName: "VARAGON",
+    projectDesc:
+      "Better solutions for borrowers and investors in middle market credit",
+    tags: ["HTML", "CSS", "Vue.js", "CSS Animation"],
+    code: "",
+    demo: "https://varagon.com/",
+    image: varagon,
+  },
+  {
+    id: 8,
+    projectName: "Industry 4.0",
+    projectDesc: "Sharing digital ecosystems globally",
+    tags: ["Vue", "TypeScript"],
+    code: "",
+    demo: "https://industrie40.isometric.site/",
+    image: industrie,
+  },
+  {
+    id: 2,
+    projectName: "Lift & Co",
+    projectDesc: "Cannabis Reviews, Rewards, Events & More",
+    tags: ["React", "Node", "Semantic UI", "Contentful"],
+    code: "",
+    demo: "https://lift.co/",
+    image: liftco,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
