e6a74a26e5cbbd6a564e687eb7358a76a9c243ac
jacklindev <jackliuprodev@gmail.com>
2023-08-04 17:09:37 -0400

    update resume and social links

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     "Hello! I am Jack Alderson, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
   image: "/me.JPG",
   resumePdf:
-    "https://drive.google.com/file/d/1ahZbcMvBrubaxl99ascN7tHqnLvTYd4N/view",
+    "https://drive.google.com/file/d/16cU3XInUhvUTFc8MO-6mNOnKTcaIxAli/view",
 };
