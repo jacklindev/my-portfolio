c04d7720673605103e80f467b825f81881c9e867
jacklindev <jackliuprodev@gmail.com>
2023-08-04 16:56:41 -0400

    update name and social links

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
 export const headerData = {
-  name: "Jack Lin",
+  name: "Jack Alderson",
   title: "Hello! I am",
   desciption:
-    "Hello! I am Jack Lin, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    "Hello! I am Jack Alderson, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
   image: "/me.JPG",
   resumePdf:
     "https://drive.google.com/file/d/1ahZbcMvBrubaxl99ascN7tHqnLvTYd4N/view",
